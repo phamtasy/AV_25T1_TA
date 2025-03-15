9219d51091faae0dba0ad966331b4dcd5c56a546
Daniel <z5367956@ad.unsw.edu.au>
2025-03-15 21:22:39 +1100

    Finished Question 3

--- Question-3/Question-3.cc
+++ Question-3/Question-3.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+
+using namespace std;
 
 // Question 3: This is an extension task that requires you to decode sensor data from a CAN log file.
 // CAN (Controller Area Network) is a communication standard used in automotive applications (including Redback cars)
@@ -20,3 +23,50 @@
 // Resources:
 // https://www.csselectronics.com/pages/can-bus-simple-intro-tutorial
 // https://www.csselectronics.com/pages/can-dbc-file-database-intro
+
+// WheelSpeedRR value
+// SG_ WheelSpeedRR : 32|16@0+ (0.1,0) [0|0] "km/h" Vector__XXX
+// Bit start at 32, length 16 (bits 32 - 47), scale 0.1, offset 0, min 0, max 0, unit km/h
+// Data is in big-endian format
+// BO_ 1797 ECU_WheelSpeed: 8 Vector__XXX
+// can ID is 1797
+
+int main() {
+    // Open files
+    ofstream output_file("../Question-3/output.txt");
+    fstream input_file("../Question-3/candump.log");
+    string line;
+
+    // Loop through the input file
+    while (getline(input_file, line)) {
+        // Extract timestamp, interface, can_id, and data from the line
+        istringstream iss(line);
+        string timestamp, interface, can_id, data;
+        iss >> timestamp >> interface >> data;
+
+        // Split can_id and data
+        size_t hash_pos = data.find("#");
+        if (hash_pos == string::npos) {
+            continue;
+        }
+
+        can_id = data.substr(0, hash_pos);
+        data = data.substr(hash_pos + 1);
+
+        // Convert can_id to decimal and filter out for correct can_id (1797)
+        if (stoul(can_id, nullptr, 16) != 1797) {
+            continue;
+        }
+
+        // Convert data to decimal and extract WheelSpeedRR value (data is in big-endian format)
+        unsigned int wheelSpeedRR = std::stoul(data.substr(8, 4), nullptr, 16);
+
+        output_file << timestamp << ": " << wheelSpeedRR * 0.1 << endl;
+    }
+
+    // Close files
+    input_file.close();
+    output_file.close();
+
+    return 0;
+}
